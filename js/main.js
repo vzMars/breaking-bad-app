06f0f56fe07b14474eb41a1fb6150c3abc04c2e0
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-06-30 00:09:29 -0400

    able to render random characters on to the page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('start');
+const character = document.querySelector('.character');
+const randomBtn = document.querySelector('.randomBtn');
+const characterName = document.querySelector('h1');
+const occupations = document.querySelector('.occupations');
+const characterImg = document.querySelector('.character-img');
+const characterStatus = document.querySelector('.status');
+
+randomBtn.addEventListener('click', () => {
+  fetch('https://www.breakingbadapi.com/api/character/random')
+    .then((res) => res.json())
+    .then((data) => {
+      renderCharacter(data[0]);
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    });
+});
+
+const renderCharacter = (character) => {
+  characterName.textContent = `${character.name} aka ${character.nickname}`;
+  occupations.textContent = character.occupation.join(',');
+  characterImg.src = character.img;
+  characterImg.style.border = '15px solid #c1b826';
+  characterStatus.textContent = character.status;
+};
